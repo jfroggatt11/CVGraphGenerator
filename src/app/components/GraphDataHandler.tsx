94735bacd2bbfb2408d8f017d0d13dc0ae3cb961
Jonah <jonahfroggatt@gmail.com>
2025-05-15 20:01:28 +0200

    bug fixing parquet files

--- src/app/components/GraphDataHandler.tsx
+++ src/app/components/GraphDataHandler.tsx
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import GraphViewer from "./GraphViewer";
 import { Box, Container, Tab, Tabs } from "@mui/material";
-import { useDropzone } from "react-dropzone";
-import DropZone from "./DropZone";
-import Introduction from "./Introduction";
 import useFileHandler from "../hooks/useFileHandler";
 import useGraphData from "../hooks/useGraphData";
 import DataTableContainer from "./DataTableContainer";
@@ -39,7 +36,6 @@ const GraphDataHandler: React.FC = () => {
     communities,
     covariates,
     communityReports,
-    handleFilesRead,
     loadDefaultFiles,
   } = useFileHandler();
 
@@ -64,7 +60,8 @@ const GraphDataHandler: React.FC = () => {
 
   useEffect(() => {
     loadDefaultFiles();
-  }, [loadDefaultFiles]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
@@ -76,42 +73,22 @@ const GraphDataHandler: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    // **Set tab index based on the current path**
     switch (location.pathname) {
-      case "/upload":
-        setTabIndex(0);
-        break;
       case "/graph":
-        setTabIndex(1);
+        setTabIndex(0);
         break;
       case "/data":
-        setTabIndex(2);
+        setTabIndex(1);
         break;
       default:
-        setTabIndex(0);
+        navigate("/graph", { replace: true });
     }
-  }, [location.pathname]);
+  }, [location.pathname, navigate]);
 
-  const onDrop = (acceptedFiles: File[]) => {
-    handleFilesRead(acceptedFiles);
-    navigate("/graph", { replace: true });
-  };
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop,
-    noClick: false,
-    noKeyboard: true,
-    accept: {
-      "application/x-parquet": [".parquet"],
-    },
-  });
-
-  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+  const handleChange = (_: any, newValue: number) => {
     setTabIndex(newValue);
-    let path = "/upload";
-    if (newValue === 1) path = "/graph";
-    if (newValue === 2) path = "/data";
-    navigate(path);
+    const path = newValue === 1 ? "/data" : "/graph";
+    navigate(path, { replace: true });
     ReactGA.send({
       hitType: "event",
       eventCategory: "Tabs",
@@ -131,24 +108,10 @@ const GraphDataHandler: React.FC = () => {
   return (
     <>
       <Tabs value={tabIndex} onChange={handleChange} centered>
-        <Tab label="Upload Artifacts" />
         <Tab label="Graph Visualization" />
         <Tab label="Data Tables" />
       </Tabs>
       {tabIndex === 0 && (
-        <Container
-          maxWidth="md"
-          sx={{
-            mt: 3,
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <DropZone {...{ getRootProps, getInputProps, isDragActive }} />
-          <Introduction />
-        </Container>
-      )}
-      {tabIndex === 1 && (
         <Box
           p={3}
           sx={{
@@ -190,7 +153,7 @@ const GraphDataHandler: React.FC = () => {
           />
         </Box>
       )}
-      {tabIndex === 2 && (
+      {tabIndex === 1 && (
         <Box sx={{ display: "flex", height: "calc(100vh - 64px)" }}>
           <DataTableContainer
             selectedTable={selectedTable}
